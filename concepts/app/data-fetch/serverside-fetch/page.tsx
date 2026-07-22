d3882d86ed94e54b21365e2853c9772d5b6a4099
Nipun Shrivats <nipunrawat16@gmail.com>
2026-07-22 22:23:38 +0530

    server side

--- concepts/app/data-fetch/serverside-fetch/page.tsx
+++ concepts/app/data-fetch/serverside-fetch/page.tsx
@@ -1,7 +1,32 @@
 import React from "react";
 
-const ServerSideFetch = () => {
-  return <div>ServerSideFetch</div>;
+interface products {
+  id: number;
+  title: string;
+  price: number;
+  category: string;
+}
+interface productResponse {
+  products: products[];
+  total: number;
+}
+
+async function getProducts(): Promise<productResponse> {
+  const response = await fetch("https://dummyjson.com/products");
+  if (!response.ok) {
+    throw new Error("Failed to fetch products");
+  }
+  return response.json();
+}
+const ServerSideFetch = async () => {
+  const products = await getProducts();
+  console.log(products);
+  return (
+    <div>
+      <h1>ServerSideFetch</h1>
+      <h3>{products?.total} no. of products</h3>
+    </div>
+  );
 };
 
 export default ServerSideFetch;
